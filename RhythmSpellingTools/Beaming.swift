42af73acbe294d171ae7461a3dbdbdc1a6efd1ed
James <jbean46@gmail.com>
2017-02-11 16:35:42 -0500

    Tidy up insides of Beaming init

--- RhythmSpellingTools/Beaming.swift
+++ RhythmSpellingTools/Beaming.swift
@@ -88,32 +88,30 @@ extension Beaming.Junction {
     /// - next: Next beaming value (if it exists)
     public init(_ prev: Int?, _ cur: Int, _ next: Int?) {
         
-        var ranges: [State: CountableClosedRange<Int>] = [:]
+        typealias Ranges = [State: CountableClosedRange<Int>]
         
-        switch (prev, cur, next) {
+        /// - returns: `Ranges` for a singleton value.
+        func singleton(_ cur: Int) -> Ranges {
+            return [.beamlet: 0...cur]
+        }
+        
+        /// - returns: `Ranges` for a first value.
+        func first(_ cur: Int, _ next: Int) -> Ranges {
             
-        // single element
-        case (nil, cur, nil):
-            
-            // add beamlets for all levels
-            ranges[.beamlet] = 0 ... cur
-            
-        // first element
-        case (nil, let cur, let next?):
-            
-            // start beams as necessary
-            ranges[.start] = 0 ... min(cur, next)
+            var ranges: Ranges = [.start: 0...min(cur,next)]
             
             // add beamlets if necessary
             if cur > next {
                 ranges[.beamlet] = next + 1 ... cur
             }
             
-        // middle elements
-        case (let prev?, let cur, let next?):
+            return ranges
+        }
+        
+        /// - returns: `Ranges` for a middle value.
+        func middle(_ prev: Int, _ cur: Int, _ next: Int) -> Ranges {
             
-            // maintain beams as necessary
-            ranges[.maintain] = 0 ... min(prev,cur,next)
+            var ranges: Ranges = [.maintain: 0...min(prev,cur,next)]
             
             // start new beams if necessary
             if cur > prev {
@@ -139,23 +137,42 @@ extension Beaming.Junction {
                 }
             }
             
-        // last element
-        case (let prev?, let cur, nil):
+            return ranges
+        }
+        
+        /// - returns: `Ranges` for a last value.
+        func last(_ prev: Int, _ cur: Int) -> Ranges {
             
-            // default state
-            ranges[.stop] = 0 ... min(cur,prev)
+            // stop beams
+            var ranges: Ranges = [.stop: 0...min(cur,prev)]
             
             // add beamlets if necessary
             if cur > prev {
                 ranges[.beamlet] = prev + 1 ... cur
             }
             
-        default:
-            break
+            return ranges
+        }
+        
+        /// - returns: `Ranges` for the given context.
+        func ranges(_ prev: Int?, _ cur: Int, _ next: Int?) -> Ranges {
+            
+            switch (prev, cur, next) {
+            case (nil, cur, nil):
+                return singleton(cur)
+            case (nil, let cur, let next?):
+                return first(cur, next)
+            case (let prev?, let cur, let next?):
+                return middle(prev, cur, next)
+            case (let prev?, let cur, nil):
+                return last(prev, cur)
+            default:
+                fatalError("Ill-formed context")
+            }
         }
         
         /// Apply the states to the levels in each range
-        let states: [Int: State] = ranges.reduce([:]) { accum, cur in
+        let states: [Int: State] = ranges(prev,cur,next).reduce([:]) { accum, cur in
             
             let (state, range) = cur
             
